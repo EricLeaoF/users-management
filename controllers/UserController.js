1f8b3762e23ae452f94e24c542d13eb250ad299b
eric.freitas <eric.freitas@icolabora.com>
2024-04-01 16:14:22 -0300

    validating if email already exists

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,4 @@
+var User = require("../models/User");
 class UserController {
 
   async index(req, res) {
@@ -11,17 +12,34 @@ class UserController {
     if (email == undefined) {
       res.status(400);
       res.json({ err: "Invalid param email" })
+      return;
+    }
+
+    const emailExists = await User.findEmail(email);
+
+    if (emailExists) {
+      res.status(406);
+      res.json({ err: "Email already exists" })
+      return;
     }
 
     if (name == undefined) {
       res.status(400);
       res.json({ err: "Invalid param name" })
+      return;
     }
 
     if (password == undefined) {
       res.status(400);
       res.json({ err: "Invalid param password" })
+      return;
     }
+
+    await User.findEmail(email);
+
+    await User.new(email, password, name);
+
+    res.status(200);
     res.send("Pegando o corpo da requisição");
   }
 
