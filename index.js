1ae374572d0eced91e29c1ed7a5b64ce30a7cd90
eric.freitas <eric.freitas@icolabora.com>
2025-08-07 02:37:03 -0300

    changed dotenv to first line

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 var bodyParser = require('body-parser')
 var express = require("express")
 var app = express()
 var router = require("./routes/routes")
-require('dotenv').config();
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
